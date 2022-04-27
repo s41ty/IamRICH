a2b18780faae84522c890c1d43b92d76be4ab23f
Sergey <snyuryev@gmail.com>
2022-04-27 21:47:30 +0300

    Initial

---
+++ Shared/IamRICHApp.swift
@@ -0,0 +1,17 @@
+//
+//  IamRICHApp.swift
+//  Shared
+//
+//  Created by Sergey Yuryev on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct IamRICHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
