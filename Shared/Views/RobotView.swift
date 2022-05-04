0c979bc3532db6022e9e41230fa4966b5503182c
Sergey <snyuryev@gmail.com>
2022-05-05 00:20:10 +0300

    robot view. initial

---
+++ Shared/Views/RobotView.swift
@@ -0,0 +1,20 @@
+//
+//  RobotView.swift
+//  IamRICH
+//
+//  Created by Sergey Yuryev on 04.05.2022.
+//
+
+import SwiftUI
+
+struct RobotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RobotView_Previews: PreviewProvider {
+    static var previews: some View {
+        RobotView()
+    }
+}
