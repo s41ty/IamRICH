36360414e872bdabd11a82fc45d15eb36b605c96
Sergey <snyuryev@gmail.com>
2022-05-06 14:18:46 +0300

    update account view

--- Shared/Views/AccountView.swift
+++ Shared/Views/AccountView.swift
@@ -15,6 +15,12 @@ struct AccountView: View {
     
     @ObservedObject private var account: AccountModel
     
+    @EnvironmentObject var sdk: TinkoffInvestSDK
+    
+    @State var selectedTag:Int? = nil
+    
+    @State var selectedMac = false
+    
     
     // MARK: - Init
     
@@ -27,35 +33,75 @@ struct AccountView: View {
     // MARK: - View
     
     var body: some View {
-        VStack {
-            if account.totalAmount.count > 0 {
-                Spacer()
-                Text("На вашем счету \(account.accountName):")
-                Text("\(account.totalAmount)")
+        ZStack {
+            VStack {
+                if selectedMac {
+                    RobotView(robot: RobotModel(sdk: sdk, accountId: account.accountId, isSandbox: account.isSandbox))
+                }
+                else if account.totalAmount.count > 0 {
+                    List {
+                        Section(header: Text("Счёт")) {
+                            NavigationLink(destination:Text("Всего средств: \(account.totalAmount)")) {
+                                Text("Всего средств: \(account.totalAmount)")
+                            }
+                        }
+                        Section(header: Text("Позиции")) {
+                            ForEach(account.positions) { position in
+                                NavigationLink(destination:Text(position.figi)) {
+                                    Text(position.figi)
+                                }
+                            }
+                        }
+                        Color(.clear)
+                            .frame(height: 30)
+                            .listRowBackground(Color.clear)
+                    }
+                    .listStyle(SidebarListStyle())
+                    .navigationTitle(account.accountName)
+                    .navigationViewStyle(.automatic)
+                    #if os(iOS)
+                    .navigationBarTitleDisplayMode(.inline)
+                    #endif
+                    .toolbar {
+                        if account.isSandbox {
+                            Button(action: {
+                                print("add money")
+                                account.sandboxPayIn(accountId: account.accountId, rubAmmount: 100000)
+                            }) {
+                                Text("Пополнить")
+                            }
+                        }
+                    }
+                    .refreshable {
+                        account.fetch()
+                    }
+                } else {
+                    LoadingIndicator(animation: .threeBalls, color: .blue, size: .medium)
+                }
+            }
+            VStack {
                 Spacer()
                 Button("Поднять бабла") {
                     print("make me rich")
+                    #if os(iOS)
+                    selectedTag = 1
+                    #elseif os(macOS)
+                    selectedMac = true
+                    #endif
                 }
                 .buttonStyle(RoundedButtonStyle())
                 .frame(maxWidth: 400)
-            } else {
-                LoadingIndicator(animation: .threeBalls, color: .blue, size: .medium)
+                .background(
+                    NavigationLink(
+                        destination: RobotView(robot: RobotModel(sdk: sdk, accountId: account.accountId, isSandbox: account.isSandbox)),
+                        tag: 1,
+                        selection: $selectedTag,
+                        label: { EmptyView() }
+                    )
+                )
             }
+            .padding()
+            .zIndex(1)
         }
-        .navigationTitle(account.accountName)
-        #if os(iOS)
-        .navigationBarTitleDisplayMode(.inline)
-        #endif
-        .toolbar {
-            if account.isSandbox {
-                Button(action: {
-                    print("add money")
-                    account.sandboxPayIn(accountId: account.accountId, rubAmmount: 100000)
-                }) {
-                    Text("Пополнить")
-                }
-            }
-        }
-        .padding()
     }
 }
