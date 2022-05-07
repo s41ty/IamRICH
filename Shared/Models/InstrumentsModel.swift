ff1f377b8dc9bcd338c568ad2db2420fc7f84574
Sergey <snyuryev@gmail.com>
2022-05-08 00:03:27 +0300

    add instruments model

---
+++ Shared/Models/InstrumentsModel.swift
@@ -0,0 +1,163 @@
+//
+//  InstrumentsModel.swift
+//  IamRICH
+//
+//  Created by Sergey Yuryev on 07.05.2022.
+//
+
+import Combine
+import Foundation
+import TinkoffInvestSDK
+
+public class InstrumentsModel: ObservableObject {
+    
+    // MARK: - Properties
+
+    @Published public var cached = [String: String]()
+    
+    private var cancellableSet = Set<AnyCancellable>()
+
+    private var sdk: TinkoffInvestSDK
+
+    
+    // MARK: - Init
+    
+    public init(sdk: TinkoffInvestSDK) {
+        self.sdk = sdk
+    }
+    
+    public func getInstrument(figi: String, type: String) {
+        if self.cached[figi] != nil {
+            return
+        } else if figi == "FG0000000000" {
+            self.cached[figi] = "Российский рубль"
+            return
+        }
+        
+        if type == "currency" {
+            sdk.instrumentsService.currencyBy(figi: figi)
+                .receive(on: RunLoop.main)
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                    case .finished:
+                        print("did finish loading currencyBy")
+                    }
+                } receiveValue: { [weak self] response in
+                    print(response)
+                    self?.cached[response.instrument.figi] = response.instrument.name
+                }
+                .store(in: &cancellableSet)
+        } else if type == "bond" {
+            sdk.instrumentsService.bondBy(figi: figi)
+                .receive(on: RunLoop.main)
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                    case .finished:
+                        print("did finish loading bondBy")
+                    }
+                } receiveValue: { [weak self] response in
+                    print(response)
+                    self?.cached[response.instrument.figi] = response.instrument.name
+                }
+                .store(in: &cancellableSet)
+        } else if type == "share" {
+            sdk.instrumentsService.shareBy(figi: figi)
+                .receive(on: RunLoop.main)
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                    case .finished:
+                        print("did finish loading shareBy")
+                    }
+                } receiveValue: { [weak self] response in
+                    print(response)
+                    self?.cached[response.instrument.figi] = response.instrument.name
+                }
+                .store(in: &cancellableSet)
+        } else if type == "eft" {
+            sdk.instrumentsService.etfBy(figi: figi)
+                .receive(on: RunLoop.main)
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                    case .finished:
+                        print("did finish loading etfBy")
+                    }
+                } receiveValue: { [weak self] response in
+                    print(response)
+                    self?.cached[response.instrument.figi] = response.instrument.name
+                }
+                .store(in: &cancellableSet)
+        }
+    }
+    
+    public func getEfts() {
+        sdk.instrumentsService.etfs()
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                case .finished:
+                    print("did finish loading etfs")
+                }
+            } receiveValue: { response in
+                print(response)
+            }
+            .store(in: &cancellableSet)
+    }
+    
+    public func getBonds() {
+        sdk.instrumentsService.bonds()
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                case .finished:
+                    print("did finish loading bonds")
+                }
+            } receiveValue: { response in
+                print(response)
+            }
+            .store(in: &cancellableSet)
+    }
+    
+    public func getShares() {
+        sdk.instrumentsService.shares()
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                case .finished:
+                    print("did finish loading shares")
+                }
+            } receiveValue: { response in
+                print(response)
+            }
+            .store(in: &cancellableSet)
+    }
+    
+    public func getCurrencies() {
+        sdk.instrumentsService.currencies()
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("\(error.localizedDescription) \(String(describing: error.trailingMetadata))")
+                case .finished:
+                    print("did finish loading currencies")
+                }
+            } receiveValue: { response in
+                print(response)
+            }
+            .store(in: &cancellableSet)
+    }
+}
